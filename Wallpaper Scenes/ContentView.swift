47ca5f4a5a330aba2457f91a7f723dae500deccd
Caedmon Myers <caedmonmyers@icloud.com>
2025-01-19 17:43:30 -0600

    Initial Commit

---
+++ Wallpaper Scenes/ContentView.swift
@@ -0,0 +1,27 @@
+//
+// Wallpaper Scenes
+// ContentView.swift
+//
+// Created on 19/1/25
+//
+// Copyright ©2025 DoorHinge Apps.
+//
+
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Wallpaper Scenes/Wallpaper_ScenesApp.swift
@@ -0,0 +1,20 @@
+//
+// Wallpaper Scenes
+// Wallpaper_ScenesApp.swift
+//
+// Created on 19/1/25
+//
+// Copyright ©2025 DoorHinge Apps.
+//
+
+
+import SwiftUI
+
+@main
+struct Wallpaper_ScenesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
